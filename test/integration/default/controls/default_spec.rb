0a76a8f8abf9d413259e60718213ebca17193b15
Derek Wood <derek@cask.co>
2018-05-04 02:21:10 -0700

    add docker-based integration tests

---
+++ test/integration/default/controls/default_spec.rb
@@ -0,0 +1,24 @@
+#
+
+describe directory('/opt/cdap') do
+  it { should exist }
+  it { should be_readable }
+end
+
+describe file('/opt/cdap/sandbox/conf/cdap-site.xml') do
+  it { should exist }
+end
+
+describe service('cdap-sandbox') do
+  it { should be_installed }
+  it { should be_running }
+end
+
+[
+  'ps auxww | grep -i cdap',
+  '/opt/cdap/sandbox/bin/cdap cli list namespaces | grep "Default Namespace"',
+].each do |cmd|
+  describe command(cmd) do
+    its('exit_status') { should eq 0 }
+  end
+end
